dea903eef79141a0e81e89c870492a3775866979
Ponmani P <ponmani4028@gmail.com>
2026-09-22 14:57:16 +0530

    fix: move vercel.json to project root

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Favorites from "./pages/Favorites";
 import Products from "./pages/Products";
 import Inbox from "./pages/Inbox";
 import OrderLists from "./pages/OrderLists";
-
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
+      {" "}
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/inbox" element={<Inbox />} />
-        <Route path="/orders" element={<OrderLists />} />
-      </Routes>
-    </HashRouter>
+        {" "}
+        <Route path="/" element={<Dashboard />} />{" "}
+        <Route path="/favorites" element={<Favorites />} />{" "}
+        <Route path="/products" element={<Products />} />{" "}
+        <Route path="/inbox" element={<Inbox />} />{" "}
+        <Route path="/orders" element={<OrderLists />} />{" "}
+      </Routes>{" "}
+    </BrowserRouter>
   );
 }
